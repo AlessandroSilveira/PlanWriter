38ffc84f6ff79188320f9b0fe54909b529e35ea0
AlessandroSilveira <alesilver.si@gmail.com>
2026-02-18 11:32:16 -0300

    fix(sql-init): harden EventWordWars Start/End column migration

--- docker/sql/init/001-init.sql
+++ docker/sql/init/001-init.sql
@@ -149,11 +149,8 @@ BEGIN
             ADD StartAtUtc DATETIME2 NULL;
 
         UPDATE dbo.EventWordWars
-        SET StartAtUtc = StartsAtUtc
+        SET StartAtUtc = ISNULL(StartsAtUtc, SYSUTCDATETIME())
         WHERE StartAtUtc IS NULL;
-
-        ALTER TABLE dbo.EventWordWars
-            ALTER COLUMN StartAtUtc DATETIME2 NOT NULL;
     END
 
     IF COL_LENGTH(N'dbo.EventWordWars', N'EndAtUtc') IS NULL
@@ -163,11 +160,94 @@ BEGIN
             ADD EndAtUtc DATETIME2 NULL;
 
         UPDATE dbo.EventWordWars
-        SET EndAtUtc = EndsAtUtc
+        SET EndAtUtc = ISNULL(EndsAtUtc, DATEADD(minute, ISNULL(DurationInMinutes, 0), ISNULL(StartAtUtc, SYSUTCDATETIME())))
+        WHERE EndAtUtc IS NULL;
+    END
+
+    IF COL_LENGTH(N'dbo.EventWordWars', N'StartAtUtc') IS NOT NULL
+    BEGIN
+        UPDATE dbo.EventWordWars
+        SET StartAtUtc = SYSUTCDATETIME()
+        WHERE StartAtUtc IS NULL;
+
+        IF EXISTS (
+            SELECT 1
+            FROM sys.columns
+            WHERE object_id = OBJECT_ID(N'dbo.EventWordWars')
+              AND name = N'StartAtUtc'
+              AND is_nullable = 1
+        )
+        BEGIN
+            ALTER TABLE dbo.EventWordWars
+                ALTER COLUMN StartAtUtc DATETIME2 NOT NULL;
+        END
+    END
+
+    IF COL_LENGTH(N'dbo.EventWordWars', N'EndAtUtc') IS NOT NULL
+    BEGIN
+        UPDATE dbo.EventWordWars
+        SET EndAtUtc = DATEADD(minute, ISNULL(DurationInMinutes, 0), ISNULL(StartAtUtc, SYSUTCDATETIME()))
         WHERE EndAtUtc IS NULL;
 
+        IF EXISTS (
+            SELECT 1
+            FROM sys.columns
+            WHERE object_id = OBJECT_ID(N'dbo.EventWordWars')
+              AND name = N'EndAtUtc'
+              AND is_nullable = 1
+        )
+        BEGIN
+            ALTER TABLE dbo.EventWordWars
+                ALTER COLUMN EndAtUtc DATETIME2 NOT NULL;
+        END
+    END
+
+    IF COL_LENGTH(N'dbo.EventWordWars', N'StartAtUtc') IS NULL
+       AND COL_LENGTH(N'dbo.EventWordWars', N'StartsAtUtc') IS NULL
+    BEGIN
         ALTER TABLE dbo.EventWordWars
-            ALTER COLUMN EndAtUtc DATETIME2 NOT NULL;
+            ADD StartAtUtc DATETIME2 NOT NULL
+                CONSTRAINT DF_EventWordWars_StartAtUtc_Backfill DEFAULT (SYSUTCDATETIME());
+    END
+
+    IF COL_LENGTH(N'dbo.EventWordWars', N'EndAtUtc') IS NULL
+       AND COL_LENGTH(N'dbo.EventWordWars', N'EndsAtUtc') IS NULL
+    BEGIN
+        ALTER TABLE dbo.EventWordWars
+            ADD EndAtUtc DATETIME2 NOT NULL
+                CONSTRAINT DF_EventWordWars_EndAtUtc_Backfill DEFAULT (SYSUTCDATETIME());
+
+        UPDATE dbo.EventWordWars
+        SET EndAtUtc = DATEADD(minute, ISNULL(DurationInMinutes, 0), StartAtUtc);
+
+        ALTER TABLE dbo.EventWordWars
+            DROP CONSTRAINT DF_EventWordWars_EndAtUtc_Backfill;
+    END
+
+    IF COL_LENGTH(N'dbo.EventWordWars', N'StartAtUtc') IS NOT NULL
+       AND COL_LENGTH(N'dbo.EventWordWars', N'EndAtUtc') IS NOT NULL
+    BEGIN
+        IF EXISTS (
+            SELECT 1
+            FROM sys.default_constraints
+            WHERE parent_object_id = OBJECT_ID(N'dbo.EventWordWars')
+              AND name = N'DF_EventWordWars_StartAtUtc_Backfill'
+        )
+        BEGIN
+            ALTER TABLE dbo.EventWordWars
+                DROP CONSTRAINT DF_EventWordWars_StartAtUtc_Backfill;
+        END
+
+        IF EXISTS (
+            SELECT 1
+            FROM sys.default_constraints
+            WHERE parent_object_id = OBJECT_ID(N'dbo.EventWordWars')
+              AND name = N'DF_EventWordWars_EndAtUtc_Backfill'
+        )
+        BEGIN
+            ALTER TABLE dbo.EventWordWars
+                DROP CONSTRAINT DF_EventWordWars_EndAtUtc_Backfill;
+        END
     END
 END
 GO
